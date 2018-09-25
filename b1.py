3c9794a101718b858f63b2f484c6a8b519925ef2
Trieu210 <vanoss010@gmail.com>
2018-09-25 20:24:38 +0700

    HOMEWORK

---
+++ b1.py
@@ -0,0 +1,15 @@
+x = int(input('Pls type a month u like'))
+if x < 2:
+    print('winter')
+if 2 < x < 5:
+    print('spring')
+
+if 5 < x < 8:
+    print('summer')
+
+if  8 < x < 11:
+    print('autumn')
+
+elif 11< x < 13:
+    print("winter")
+    
